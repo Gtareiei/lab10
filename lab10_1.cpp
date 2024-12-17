7be77ceb6c7473fdde7235cfdbf0cb29995fca82
G <G@gmail.com>
2024-12-17 16:26:58 +0700

    lab10_1com

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -2,25 +2,48 @@
 using namespace std;
 
 int main(){
-	int count[5] = {}; //Declare array count for counting A,B,C,D,F and initialize all element = 0
-	cout << "Please input grade of each student (A-F) or input 0 to exit.";
+	int count[5] = {'A' ,'B' ,'C' ,'D' ,'F'};
+	int A=0 ,B=0,C=0,D=0,F=0 ;
+	char grade ;
+	int sum=1 ;                    //Declare array count for counting A,B,C,D,F and initialize all element = 0
+	cout << "Please input grade of each student (A-F) or input 0 to exit. \n";
 	do{
-		cout << "Student [" << "]: ";
-		cin >> grade; //The loop must be terminated when grade = '0'
-		if(true) // if grade is A
+		cout << "Student [" <<sum<< "]: ";
+		cin >> grade; 
+		    //cout<<grade ;
+		if(grade=='0')
+		{break ;} 
+		                          //The loop must be terminated when grade = '0'
+		
+		if(grade==count[0]){                                      // if grade is A
+			A++ ;
+			sum+=1;}
 			//Do something
-		}else if(true) // if grade is B
-			//Do something
-		//and so on ... for grade = C, D, F	
-		}else{ // grade is wrong input
-			//Do something
+		else if(grade==count[1]){  
+		    B++ ;
+		    sum+=1 ;}                                     // if grade is B
+	    else if (grade==count[2]){
+		    C++  ;                
+		    sum+=1 ;} //Do something
+	    else if (grade==count[3]){
+	        D++ ;   
+	        sum+=1 ;}
+	    else if(grade==count[4]) {                   //and so on ... for grade = C, D, F	
+	        F++ ;
+	        sum+=1 ;}
+		else{                                  // grade is wrong input
+		   cout<< "Wrong input. Please input again. \n" ;  
+		   //Do something
 		} 
 	}while(true);
 	
 	
-	cout << "In total ? students.";
-	cout << "A = " << count[0] <<", ";
-	cout << "B = " << count[1] <<", ";	
+	cout << "In total "<<sum-1<< " students. \n";
+	cout << "A = " << A <<", ";
+	cout << "B = " << B <<", ";	
+	cout << "C = " << C <<", ";
+	cout << "D = " << D <<", ";
+	cout << "F = " << F ;
 	//	and so on ... for grade = C, D, F	
 	
 	return 0;
